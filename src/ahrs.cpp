2d855933c05a017d0fb410479a6a53eb171a80cb
JeffreyZhuang <jeffreyzhuangyt@gmail.com>
2024-11-17 20:19:36 -0500

    ahrs

--- src/ahrs.cpp
+++ src/ahrs.cpp
@@ -5,9 +5,14 @@ AHRS::AHRS(Vehicle * vehicle) {
 }
 
 void AHRS::setup() {
-    filter.begin(100);
+    filter.begin(200);
 }
 
 void AHRS::update() {
-    
+    filter.update(_vehicle->imu_gx, _vehicle->imu_gy, _vehicle->imu_gz, 
+                  _vehicle->imu_ax, _vehicle->imu_ay, _vehicle->imu_az, 
+                  _vehicle->compass_mx, _vehicle->compass_my, _vehicle->compass_mz);
+    _vehicle->ahrs_roll = filter.getRoll();
+    _vehicle->ahrs_pitch = filter.getPitch();
+    _vehicle->ahrs_yaw = filter.getYaw();
 }
--- src/main.cpp
+++ src/main.cpp
@@ -36,14 +36,15 @@ void loop() {
   if (micros() - prev_print_time > 300000) {
     // Replace with USB instead of SWO
     // swo.println("Time: " + String(millis()));
-    swo.println(String(vehicle.baro_alt) + "\t" + String(vehicle.imu_ax) + "\t" + String(vehicle.imu_ay) + 
-                "\t" + String(vehicle.imu_gz) + "\t" + String(vehicle.compass_mx) + "\t" + 
-                String(vehicle.compass_my) + "\t" + String(vehicle.compass_mz));
+    // swo.println(String(vehicle.baro_alt) + "\t" + String(vehicle.imu_ax) + "\t" + String(vehicle.imu_ay) + 
+    //             "\t" + String(vehicle.imu_gz) + "\t" + String(vehicle.compass_mx) + "\t" + 
+    //             String(vehicle.compass_my) + "\t" + String(vehicle.compass_mz));
     // swo.println(vehicle.autopilot_voltage, 6);
     // swo.println(vehicle.autopilot_current, 6);
     // swo.println(vehicle.batt_voltage, 6);
     // swo.println(vehicle.batt_current, 6);
-    swo.println("dt: " + String((micros() - prev_loop_time) / 1000));
+    // swo.println("dt: " + String((micros() - prev_loop_time) / 1000));
+    swo.println(String(vehicle.ahrs_roll) + "\t" + String(vehicle.ahrs_pitch) + "\t" + String(vehicle.ahrs_yaw));
     prev_print_time = micros();
   }
   
