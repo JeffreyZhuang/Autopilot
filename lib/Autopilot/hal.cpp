4042f5dfe3b1465b5468544caf9856e5d79b75bc
JeffreyZhuang <jeffreyzhuangyt@gmail.com>
2024-11-30 01:11:55 -0500

    a

--- lib/Autopilot/hal.cpp
+++ lib/Autopilot/hal.cpp
@@ -1,40 +1,40 @@
-#include <arduino_hal.h>
+#include <hal.h>
 
-Arduino_HAL::Arduino_HAL(Plane * plane): HAL(plane), spi_bus(PB5, PB4, PA5), imu(spi_bus, PC15), baro(PC14, &spi_bus, 20000000, OSR_ULTRA_HIGH), 
+HAL::HAL(Plane * plane): spi_bus(PB5, PB4, PA5), imu(spi_bus, PC15), baro(PC14, &spi_bus, 20000000, OSR_ULTRA_HIGH), 
                                          i2c_bus(PB9, PB8), ina219(&i2c_bus, 0x40, 0.01), swo(2000000) {
     _plane = plane;
 }
 
-void Arduino_HAL::delay_us(uint32_t us) {
+void HAL::delay_us(uint32_t us) {
     delayMicroseconds(us);
 }
 
-void Arduino_HAL::swo_print(char * str) {
+void HAL::swo_print(char * str) {
     swo.print(str);
 }
 
-void Arduino_HAL::usb_print(char * str) {
+void HAL::usb_print(char * str) {
     Serial.print(str);
 }
 
-uint32_t Arduino_HAL::get_time_us() {
+uint32_t HAL::get_time_us() {
     return micros();
 }
 
-void Arduino_HAL::blink_led() {
+void HAL::blink_led() {
     digitalWrite(PC1, HIGH);
     delay_us(500000);
     digitalWrite(PC1, LOW);
     delay_us(500000);
 }
 
-void Arduino_HAL::setup() {
+void HAL::setup() {
     setup_sd(); // BUG: Datalog setup does not work when moved under i2c_begin. I haven't defined the SDIO pins, maybe pins override i2c?
     setup_peripherals();
     setup_sensors();
 }
 
-void Arduino_HAL::setup_peripherals() {
+void HAL::setup_peripherals() {
     Serial.begin(115200);
     pinMode(PC1, OUTPUT);
 
@@ -42,13 +42,13 @@ void Arduino_HAL::setup_peripherals() {
     i2c_bus.setClock(100000);
 }
 
-void Arduino_HAL::setup_sensors() {
+void HAL::setup_sensors() {
     imu.begin();
     baro.begin();
     mag.begin_SPI(PC13, &spi_bus);
 }
 
-void Arduino_HAL::setup_sd() {
+void HAL::setup_sd() {
     swo.println("Initizliaing SD card...");
     while (!SD.begin(SD_DETECT_NONE)) {
         delay(10);
@@ -61,19 +61,19 @@ void Arduino_HAL::setup_sd() {
     }
 }
 
-void Arduino_HAL::write_sd() {
+void HAL::write_sd() {
     file.println(String(micros()) + "," + String(_plane->baro_alt) + "," + String(_plane->imu_az));
     file.flush();
 }
 
-void Arduino_HAL::poll() {
+void HAL::poll() {
     poll_imu();
     poll_compass();
     poll_barometer();
     poll_power_monitor();
 }
 
-void Arduino_HAL::poll_imu() {
+void HAL::poll_imu() {
     imu.getAGT();
     _plane->imu_ax = imu.accX();
     _plane->imu_ay = imu.accY();
@@ -85,17 +85,17 @@ void Arduino_HAL::poll_imu() {
     // Insert code here to rotate IMU data into correct frame, maybe imu_correction() function
 }
 
-void Arduino_HAL::poll_compass() {
+void HAL::poll_compass() {
     mag.readDataNonBlocking(&_plane->compass_mx, &_plane->compass_my, &_plane->compass_mz);
 }
 
-void Arduino_HAL::poll_barometer() {
+void HAL::poll_barometer() {
     if (baro.read()) {
         _plane->baro_alt = (pow(1013.25/baro.getPressure(), 1.0 / 5.257) - 1.0) * (baro.getTemperature() + 273.15) / 0.0065;
     }
 }
 
-void Arduino_HAL::poll_power_monitor() {
+void HAL::poll_power_monitor() {
     _plane->batt_voltage = analogRead(PC0) * (3.3 / 1023.0);
     _plane->batt_current = analogRead(PC2) * (3.3 / 1023.0);
     _plane->autopilot_voltage = ina219.read_voltage();
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include <Arduino.h>
 #include <autopilot.h>
 
 Autopilot autopilot;
