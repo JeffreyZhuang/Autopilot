c7599797540e3c416b5e8403e24c948ad3230697
JeffreyZhuang <jeffreyzhuangyt@gmail.com>
2025-01-29 02:36:23 -0500

    a

--- Autopilot/parameters.h
+++ Autopilot/parameters.h
@@ -8,11 +8,22 @@
 #ifndef PARAMETERS_H_
 #define PARAMETERS_H_
 
-// Limits
-constexpr float AIRSPEED_CRUISE = 20; // Meters per second, not knots!
-constexpr float TRIM_THROTTLE = 0.3; // Do I even need this? I can use integrator?
+// Airspeed
+constexpr float AIRSPEED_CRUISE = 20; // Meters per second
+
+// Throttle
+constexpr float TRIM_THROTTLE = 0.3; // Steady-state cruise throttle
+constexpr float THR_MIN = 0; // Maximum throttle between 0 and 1
+constexpr float THR_MAX = 1; // Minimum throttle between 0 and 1
+
+// Attitude
 constexpr float PTCH_LIM_MAX_DEG = 10;
 constexpr float PTCH_LIM_MIN_DEG = -10;
 constexpr float ROLL_LIM_DEG = 10;
+constexpr bool RUDDER_ONLY = true;
+
+// Takeoff
+constexpr float TAKEOFF_ALT = 10; // Altitude that the plane will climb to during takeoff meters per second
+constexpr float TAKEOFF_THR = 1; // Throttle set during takeoff between 0 and 1
 
 #endif /* PARAMETERS_H_ */
