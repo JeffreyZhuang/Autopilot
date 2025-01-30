4bdae62b8d96608ba774b2bb568f027d0d34315d
JeffreyZhuang <jeffreyzhuangyt@gmail.com>
2025-01-30 12:05:27 -0500

    a

--- Autopilot/plane.h
+++ Autopilot/plane.h
@@ -24,10 +24,10 @@ enum class AutoMode
 struct Plane
 {
     // Monitor
-    float batt_current;
-    float batt_voltage;
-    float autopilot_current;
-    float autopilot_voltage;
+    float batt_current = 0;
+    float batt_voltage = 0;
+    float autopilot_current = 0;
+    float autopilot_voltage = 0;
 
     // State machine
     ManualMode manualMode;
@@ -39,19 +39,19 @@ struct Plane
     uint32_t loop_iteration;
 
     // IMU
-    float imu_ax;
-    float imu_ay;
-    float imu_az;
-    float imu_gx;
-    float imu_gy;
-    float imu_gz;
+    float imu_ax = 0;
+    float imu_ay = 0;
+    float imu_az = 0;
+    float imu_gx = 0;
+    float imu_gy = 0;
+    float imu_gz = 0;
     float imu_temp;
     uint32_t imu_timestamp;
 
     // Compass
-    float compass_mx;
-    float compass_my;
-    float compass_mz;
+    float compass_mx = 0;
+    float compass_my = 0;
+    float compass_mz = 0;
     uint64_t compass_timestamp;
 
     // Barometer
@@ -61,47 +61,47 @@ struct Plane
     uint64_t baro_timestamp;
 
     // GNSS
-    float gnss_lat;
-    float gnss_lon;
-    float gnss_asl;
+    float gnss_lat = 0;
+    float gnss_lon = 0;
+    float gnss_asl = 0;
     uint8_t gnss_sats = 0;
-    float gnss_center_lat;
-    float gnss_center_lon;
-    bool gnss_lock;
+    float gnss_center_lat = 0;
+    float gnss_center_lon = 0;
+    bool gnss_lock = false;
     uint64_t gnss_timestamp;
 
     // AHRS
-    float ahrs_roll;
-    float ahrs_pitch;
-    float ahrs_yaw;
-    float ahrs_q0;
-    float ahrs_q1;
-    float ahrs_q2;
-    float ahrs_q3;
+    float ahrs_roll = 0;
+    float ahrs_pitch = 0;
+    float ahrs_yaw = 0;
+    float ahrs_q0 = 0;
+    float ahrs_q1 = 0;
+    float ahrs_q2 = 0;
+    float ahrs_q3 = 0;
     uint64_t ahrs_timestamp;
 
     // Navigation
-    float nav_pos_north;
-    float nav_pos_east;
-    float nav_pos_down;
-    float nav_vel_north;
-    float nav_vel_east;
-    float nav_vel_down;
-    float nav_acc_north;
-    float nav_acc_east;
-    float nav_acc_down;
-    float nav_airspeed;
+    float nav_pos_north = 0;
+    float nav_pos_east = 0;
+    float nav_pos_down = 0;
+    float nav_vel_north = 0;
+    float nav_vel_east = 0;
+    float nav_vel_down = 0;
+    float nav_acc_north = 0;
+    float nav_acc_east = 0;
+    float nav_acc_down = 0;
+    float nav_airspeed = 0;
     uint64_t nav_timestamp;
 
     // Guidance
-    float guidance_n_setpoint;
-    float guidance_e_setpoint;
-    float guidance_d_setpoint;
+    float guidance_n_setpoint = 0;
+    float guidance_e_setpoint = 0;
+    float guidance_d_setpoint = 0;
 
     // RC
-    float rc_rudder;
-    float rc_elevator;
-    float rc_throttle;
+    float rc_rudder = 0;
+    float rc_elevator = 0;
+    float rc_throttle = 0;
     bool rc_switch; // Change to manual_sw
 };
 
