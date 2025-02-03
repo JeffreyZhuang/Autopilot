33175badd1488945009357d4a17eb5b7d9ae9127
JeffreyZhuang <jeffreyzhuangyt@gmail.com>
2025-02-02 21:01:43 -0500

    a

--- Core/Src/PITL_HAL/telemetry_pitl.cpp
+++ Core/Src/PITL_HAL/telemetry_pitl.cpp
@@ -11,7 +11,7 @@ void Pitl_hal::read_rc()
 	_plane->rc_throttle = clamp(((int)mlrs_rc.rc_data[2] - 1000) / 1000.0f, 0, 1);
 
 	_plane->manual_sw = mlrs_rc.rc_data[4] > 1500;
-	_plane->mode_sw = mlrs_rc.rc_data[4] > 1500;
+	_plane->mode_sw = mlrs_rc.rc_data[5] > 1500;
 }
 
 void Pitl_hal::transmit_telem(uint8_t tx_buff[], int len)
