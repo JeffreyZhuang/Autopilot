da58ab22eff7c46baa3c9042b79738405e1f6251
JeffreyZhuang <jeffreyzhuangyt@gmail.com>
2025-02-05 21:38:05 -0500

    a

--- Autopilot/parameters.h
+++ Autopilot/parameters.h
@@ -26,7 +26,7 @@ constexpr bool RUDDER_ONLY = false;
 
 // Autoland
 constexpr float LAND_GS_DEG = 10; // Landing glideslope angle
-constexpr float LAND_FLARE_ALT = 35; // Flare altitude
+constexpr float LAND_FLARE_ALT = 25; // Flare altitude
 constexpr float FLARE_SINK_RATE = 0.5;
 
 // Takeoff
