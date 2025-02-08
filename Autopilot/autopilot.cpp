101646ce999576aac5ff675acdafa5cb07954c93
JeffreyZhuang <jeffreyzhuangyt@gmail.com>
2025-02-08 01:13:33 -0500

    a

--- Autopilot/autopilot.cpp
+++ Autopilot/autopilot.cpp
@@ -78,9 +78,9 @@ void Autopilot::boot()
 	{
 		_plane->center_lat = _plane->gnss_lat;
 		_plane->center_lon = _plane->gnss_lon;
-
-		_navigation.execute();
 	}
+
+	_navigation.execute();
 
 	bool transmitter_safe = (_plane->rc_throttle < 0.1) && (_plane->manual_sw == false) && (_plane->mode_sw == false);
 	if (_plane->fix_quality == 1 && transmitter_safe)
