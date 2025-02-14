ff196711b18cc27df7dee71fb92e9b223f611c0b
JeffreyZhuang <jeffreyzhuangyt@gmail.com>
2025-02-14 14:08:10 -0500

    a

--- Autopilot/Modules/ControlAllocator/control_allocator.h
+++ Autopilot/Modules/ControlAllocator/control_allocator.h
@@ -11,6 +11,8 @@
 #include "Lib/Utils/utils.h"
 #include "hal.h"
 
+// The attitude controller works using a cascaded loop method. The outer loop computes the error between the attitude setpoint and the estimated attitude that, multiplied by a gain (P controller), generates a rate setpoint. The inner loop then computes the error in rates and uses a PI (proportional + integral) controller to generate the desired angular acceleration.
+
 class Control_allocator
 {
 public:
