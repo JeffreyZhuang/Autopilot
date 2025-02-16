5c5d43f300f4f6a9391e996a68b253822dea0c9b
JeffreyZhuang <jeffreyzhuangyt@gmail.com>
2025-02-16 00:25:05 -0500

    a

--- Core/Inc/Drivers/sd.h
+++ Core/Inc/Drivers/sd.h
@@ -12,25 +12,38 @@ class Sd
 {
 public:
 	void initialize();
-	void write();
-	void append_buffer(uint8_t* packet, uint16_t size);
-	void append_byte(uint8_t byte);
+	void write(uint8_t* tx_buff, uint16_t size);
 	void read(uint8_t* rx_buff, uint16_t size);
+	void flush();
 private:
 	FATFS fatfs;
 	FIL fil;
-
 	char filename[50];
-
-	// Double buffering approach
-	static constexpr uint32_t buffer_max_len = 4096;
-	uint8_t back_buffer[buffer_max_len];
-	uint8_t front_buffer[buffer_max_len];
-	uint8_t new_buffer[buffer_max_len];
-	bool front_buff_full = false;
-	uint32_t back_buff_last_idx = 0;
-
-	bool closed = false; // Flag for closing the file before reading
 };
+
+//class Sd
+//{
+//public:
+//	void initialize();
+//	void write();
+//	void append_buffer(uint8_t* packet, uint16_t size);
+//	void append_byte(uint8_t byte);
+//	void read(uint8_t* rx_buff, uint16_t size);
+//private:
+//	FATFS fatfs;
+//	FIL fil;
+//
+//	char filename[50];
+//
+//	// Double buffering approach
+//	static constexpr uint32_t buffer_max_len = 4096;
+//	uint8_t back_buffer[buffer_max_len];
+//	uint8_t front_buffer[buffer_max_len];
+//	uint8_t new_buffer[buffer_max_len];
+//	bool front_buff_full = false;
+//	uint32_t back_buff_last_idx = 0;
+//
+//	bool closed = false; // Flag for closing the file before reading
+//};
 
 #endif /* INC_SD_H_ */
--- Core/Src/Flight_HAL/logger_hal.cpp
+++ Core/Src/Flight_HAL/logger_hal.cpp
@@ -7,12 +7,12 @@ void Flight_hal::init_logger()
 
 void Flight_hal::write_storage_buffer(uint8_t* packet, uint16_t len)
 {
-	_sd.append_buffer(packet, len);
+	_sd.write(packet, len);
 }
 
 void Flight_hal::flush_storage_buffer()
 {
-	_sd.write();
+	_sd.flush();
 }
 
 void Flight_hal::read_storage(uint8_t* rx_buff, uint16_t size)
