d47daa69b5c0393e603506b7aa27f3b648aa9064
JeffreyZhuang <jeffreyzhuangyt@gmail.com>
2025-06-08 22:59:09 -0400

    integral anti-windup

--- Autopilot/lib/tecs/tecs.cpp
+++ Autopilot/lib/tecs/tecs.cpp
@@ -1,14 +1,14 @@
 #include "tecs.h"
 
-void TECS::update(float alt_m, float vel_mps, float target_alt_m, float target_vel_mps)
+void TECS::update(float alt_m, float vel_mps, float target_alt_m, float target_vel_mps, float dt)
 {
 	const State state{alt_m, vel_mps};
 	const Setpoint setpoint{target_alt_m, target_vel_mps};
 
 	SpecificEnergies specific_energies = calc_specific_energies(state, setpoint, _param);
 
-	calc_pitch_control(specific_energies, _param);
-	calc_throttle_control(specific_energies, _param);
+	calc_pitch_control(dt, specific_energies, _param);
+	calc_throttle_control(dt, specific_energies, _param);
 }
 
 TECS::SpecificEnergies TECS::calc_specific_energies(const State state, const Setpoint setpoint, const Param param)
@@ -36,30 +36,41 @@ TECS::ControlValues TECS::calc_energy_balance(const SpecificEnergies specific_en
 	 * A weighting of 0 gives 100% priority to speed control
 	 * A weighting of 2 gives 100% priority to altitude control
 	 */
-	energy_balance.estimate = specific_energies.potential.estimate * param.alt_weight -
-							  specific_energies.kinetic.estimate * (2.0 - param.alt_weight);
+	energy_balance.estimate = specific_energies.potential.estimate * param.alt_weight - specific_energies.kinetic.estimate * (2.0 - param.alt_weight);
 
-	energy_balance.setpoint = specific_energies.potential.setpoint * param.alt_weight -
-							  specific_energies.kinetic.setpoint * (2.0 - param.alt_weight);
+	energy_balance.setpoint = specific_energies.potential.setpoint * param.alt_weight - specific_energies.kinetic.setpoint * (2.0 - param.alt_weight);
 
-	// Clamp total energy setpoint within allowed airspeed range to prevent stall/overspeed
+	// TODO: MOVE THIS TO ITS OWN FUNCTION (calc_energy_balance_limit) SO YOU CAN REUSE CALC_ENERGY_BALANCE FUNCTION
 	float min_kinetic_energy = 0.5 * powf(param.min_spd, 2);
 	float max_kinetic_energy = 0.5 * powf(param.max_spd, 2);
 	float min_energy_balance_setpoint = specific_energies.potential.setpoint * param.alt_weight - max_kinetic_energy * (2.0f - param.alt_weight);
 	float max_energy_balance_setpoint = param.alt_weight * specific_energies.potential.setpoint - min_kinetic_energy * (2.0f - param.alt_weight);
 
+	// Clamp total energy setpoint within allowed airspeed range to prevent stall/overspeed
 	energy_balance.setpoint = clamp(energy_balance.setpoint, min_energy_balance_setpoint, max_energy_balance_setpoint);
 
 	return energy_balance;
 }
 
-void TECS::calc_pitch_control(const SpecificEnergies specific_energies, const Param param)
+void TECS::calc_pitch_control(float dt, const SpecificEnergies specific_energies, const Param param)
 {
 	ControlValues energy_balance = calc_energy_balance(specific_energies, param);
 
 	float error = energy_balance.setpoint - energy_balance.estimate;
 
-	_pitch_setpoint = param.pitch_gain * error;
+	float pitch_integ_input = error * dt;
+
+	// Prevent integrator propagation into direction which saturates pitch
+	if (!(((_pitch_setpoint >= param.max_pitch) && (pitch_integ_input > 0)) ||
+		  ((_pitch_setpoint <= param.min_pitch) && (pitch_integ_input < 0))))
+	{
+		_pitch_integ_state += pitch_integ_input;
+	}
+
+	float pitch_setpoint_unclamped = param.pitch_gain * error +
+									 param.pitch_integral_gain * _pitch_integ_state;
+
+	_pitch_setpoint = clamp(pitch_setpoint_unclamped, param.min_pitch, param.max_pitch);
 }
 
 TECS::ControlValues TECS::calc_total_energy(const SpecificEnergies specific_energies, const Param param)
@@ -70,20 +81,32 @@ TECS::ControlValues TECS::calc_total_energy(const SpecificEnergies specific_ener
 
 	total_energy.setpoint = specific_energies.kinetic.setpoint + specific_energies.potential.setpoint;
 
-	// Clamp total energy setpoint within allowed airspeed range to prevent stall/overspeed
 	float min_total_energy_setpoint = specific_energies.potential.setpoint + 0.5 * powf(param.min_spd, 2);
 	float max_total_energy_setpoint = specific_energies.potential.setpoint + 0.5 * powf(param.max_spd, 2);
 
+	// Clamp total energy setpoint within allowed airspeed range to prevent stall/overspeed
 	total_energy.setpoint = clamp(total_energy.setpoint, min_total_energy_setpoint, max_total_energy_setpoint);
 
 	return total_energy;
 }
 
-void TECS::calc_throttle_control(const SpecificEnergies specific_energies, const Param param)
+void TECS::calc_throttle_control(float dt, const SpecificEnergies specific_energies, const Param param)
 {
 	ControlValues total_energy = calc_total_energy(specific_energies, param);
 
 	float error = total_energy.setpoint - total_energy.estimate;
 
-	_throttle_setpoint = param.throttle_gain * error;
+	float throttle_integ_input = error * dt;
+
+	// Prevent integrator propagation into direction which saturates pitch
+	if (!(((_throttle_setpoint >= param.max_throttle) && (throttle_integ_input > 0)) ||
+		  ((_throttle_setpoint <= param.min_throttle) && (throttle_integ_input < 0))))
+	{
+		_throttle_integ_state += throttle_integ_input;
+	}
+
+	float throttle_setpoint_unclamped = param.throttle_gain * error + param.throttle_trim +
+										param.throttle_integral_gain * _throttle_integ_state;
+
+	_throttle_setpoint = clamp(throttle_setpoint_unclamped, param.min_throttle, param.max_throttle);
 }
--- Autopilot/lib/tecs/tecs.h
+++ Autopilot/lib/tecs/tecs.h
@@ -12,20 +12,34 @@ public:
 		float min_spd;
 		float max_spd;
 		float alt_weight;
+		float min_pitch;
+		float max_pitch;
 		float pitch_gain;
+		float pitch_integral_gain;
+		float min_throttle;
+		float max_throttle;
+		float throttle_trim;
 		float throttle_gain;
+		float throttle_integral_gain;
 	};
 
 	TECS() = default;
 	~TECS() = default;
 
-	void update(float alt_m, float vel_mps, float target_alt_m, float target_vel_mps);
+	void update(float alt_m, float vel_mps, float target_alt_m, float target_vel_mps, float dt);
 
 	void set_min_spd(float min_spd) { _param.min_spd = min_spd; };
 	void set_max_spd(float max_spd) { _param.max_spd = max_spd; };
 	void set_alt_weight(float alt_weight) { _param.alt_weight = alt_weight; };
+	void set_min_pitch(float min_pitch) { _param.min_pitch = min_pitch; };
+	void set_max_pitch(float max_pitch) { _param.max_pitch = max_pitch; };
 	void set_pitch_gain(float pitch_gain) { _param.pitch_gain = pitch_gain; };
+	void set_pitch_integral_gain(float pitch_integral_gain) { _param.pitch_integral_gain = pitch_integral_gain; };
+	void set_min_throttle(float min_throttle) { _param.min_throttle = min_throttle; };
+	void set_max_throttle(float max_throttle) { _param.max_throttle = max_throttle; };
+	void set_throttle_trim(float throttle_trim) { _param.throttle_trim = throttle_trim; };
 	void set_throttle_gain(float throttle_gain) { _param.throttle_gain = throttle_gain; };
+	void set_throttle_integral_gain(float throttle_integral_gain) { _param.throttle_integral_gain = throttle_integral_gain; };
 
 	float get_pitch_setpoint() { return _pitch_setpoint; };
 	float get_throttle_setpoint() { return _throttle_setpoint; };
@@ -54,13 +68,18 @@ private:
 	SpecificEnergies calc_specific_energies(const State state, const Setpoint setpoint, const Param param);
 
 	ControlValues calc_energy_balance(const SpecificEnergies specific_energies, const Param param);
-	void calc_pitch_control(const SpecificEnergies specific_energies, const Param param);
+	void calc_pitch_control(float dt, const SpecificEnergies specific_energies, const Param param);
 
 	ControlValues calc_total_energy(const SpecificEnergies specific_energies, const Param param);
-	void calc_throttle_control(const SpecificEnergies specific_energies, const Param param);
+	void calc_throttle_control(float dt, const SpecificEnergies specific_energies, const Param param);
 
 	Param _param{0};
 
+	// State
+	float _pitch_integ_state = 0;
+	float _throttle_integ_state = 0;
+
+	// Output
 	float _pitch_setpoint = 0;
 	float _throttle_setpoint = 0;
 };
